d3118f9303762df5ea179a7cf6f39a5833166c78
magghi111111 <martello2007cela@gmail.com>
2026-01-23 10:34:31 +0100

    Aggiunta query ordini e movimenti

--- backend/query/movimenti.php
+++ backend/query/movimenti.php
@@ -4,11 +4,23 @@ function getUltimiMovimenti(){
     $pdo=connect();
     $sql = "SELECT delta, tipo, note
         from movimenti
-        ORDER BY data_movimento
+        ORDER BY data_movimento DESC
         LIMIT 3;";
 
     $stmt = $pdo->query($sql);
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
+
+function getAllMovimenti(){
+    $pdo=connect();
+    $sql = "SELECT delta, tipo, note, data_movimento
+        from movimenti
+        ORDER BY data_movimento DESC;";
+
+    $stmt = $pdo->query($sql);
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+
 
 ?>
