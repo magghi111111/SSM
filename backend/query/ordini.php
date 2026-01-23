8f695afc4edc8f2e6e3f56d0169918e29ff352c3
magghi111111 <martello2007cela@gmail.com>
2026-01-23 18:20:37 +0100

    fix query ordini

--- backend/query/ordini.php
+++ backend/query/ordini.php
@@ -14,11 +14,9 @@ function getOrdiniRecenti(){
 
 function getAllOrdini(){
     $pdo=connect();
-    $sql = "SELECT o.id,o.id_shopify, DATE_FORMAT(o.data_creazione, '%d/%m/%Y') AS data_creazione,o.stato, c.nome, c.cognome,p.nome,ro.quantita
+    $sql = "SELECT o.id,o.id_shopify, DATE_FORMAT(o.data_creazione, '%d/%m/%Y') AS data_creazione,o.stato, c.nome, c.cognome
     FROM ordini o
     JOIN cliente c ON o.id_cliente = c.codice
-    JOIN righe_ordini ro ON o.id = ro.id_ordine
-    join componenti p ON ro.id_prodotto = p.id
     ORDER BY o.data_creazione DESC;";
 
     $stmt = $pdo->query($sql);
