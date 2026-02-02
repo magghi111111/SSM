1023d3687fcfc4d0b58a7b89694a8c6a62696cfb
GuglielmiAlberto <albertoguglielmi07@gmail.com>
2026-02-02 12:44:30 +0100

    Aggiornamento form

--- backend/controller/consegnaCntrl.php
+++ backend/controller/consegnaCntrl.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once '../model/database.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     
     $idFornitore = $_POST['id_fornitore'] ?? null;
--- backend/controller/movimentoCntrl.php
+++ backend/controller/movimentoCntrl.php
@@ -1,7 +1,9 @@
 <?php
 
 require_once '../model/database.php';
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 if ($_SERVER['REQUEST_METHOD'] === 'POST'){
     $id_componente   = $_POST['id_componente'] ?? null;
     if($id_componente === 'new'){
